e6f95411b207631526642b823445c2d389c36197
Pascal PERNOT <pascal.pernot@u-psud.fr>
2016-10-19 12:31:35 +0200

    Default uncertainty printing to parentheses notation, old uncPrint was transferred to UncPrint for enlarged uncertainties.

--- R/uncPrint.R
+++ R/uncPrint.R
@@ -1,10 +1,12 @@
 #'@title Printing Functions
 #'
 #'@description Print results with prescribed number of
-#'digits.
+#'digits following GUM recommendations.
 #'
 #'\code{uncPrint()} is used to print the mean value and
-#'(enlarged) standard uncertainty.
+#'standard uncertainty.
+#'\code{UncPrint()} is used to print the mean value and
+#'enlarged uncertainty.
 #'\code{CIPrint()} is used to print coverage intervals 
 #'from mean value, standard uncertainty and coverage factor.
 #'\code{CIPrint1()} is used to print coverage intervals 
@@ -19,7 +21,7 @@
 #'@return A character string.
 
 #'  
-#' @aliases uncPrint CIPrint CIPrint1
+#' @aliases uncPrint UncPrint CIPrint CIPrint1
 
 #' @rdname gumPrint
 #' @examples
@@ -30,8 +32,44 @@
 #' S=gumS1(fExpr,x.mu,x.u,x.pdf,x.df=NULL,nrunMax=1000)
 #' uncPrint(S$y.mu,S$y.u)
 #' @export
-uncPrint = function(y, uy) {
-  # Controlled precision output (2 digits for uy 
+uncPrint = function(y, uy, numDig=2) {
+  # Print result + uncertainty in parenthesis format 
+  
+  if (uy <= 0) {
+    cat('Sorry: cannot pretty print with negative or null uncertainty !\n')
+    return
+  }
+  
+  # Get scales
+  n0=1+floor(log10(abs(y)))
+  n1=floor(log10(uy))
+  
+  # Format uncertainty
+  switch(sign(n1) + 2, # Map (-1,0,1) to (1,2,3)
+         {
+           fmt = paste0("%",n0-n1+numDig-1,".",-n1+numDig-1,"f")
+           short_uy = signif(uy/10^(n1-numDig+1),numDig)
+         },
+         {
+           fmt = paste0("%",n0-n1+numDig-1,".",-n1+numDig-1,"f")
+           short_uy = signif(uy/10^n1,numDig)
+         },
+         {    
+           fmt = paste0("%",n0,".0f")
+           short_uy = signif(uy/10^(n1-numDig+1),numDig)
+         }
+  )
+  short_y  = sprintf(fmt, y)
+  
+  cat(paste0('Y = ',short_y,'(',short_uy,')'))
+}
+#' @rdname gumPrint
+#' @examples
+#' #
+#' UncPrint(S$y.mu,2*S$y.u,fac=2)
+#' @export
+UncPrint = function(y, uy, fac=2) {
+  # Controlled precision output of enlarged uncertainty (2 digits for uy 
   # and truncate y accordingly)
   if (uy <= 0) {
     cat('Sorry: cannot pretty print with negative or null uncertainty !\n')
@@ -41,16 +79,19 @@ uncPrint = function(y, uy) {
   short_y=round(y/10^ns,2)
   short_uy=round(uy/10^ns,2)
   cat('\n')
-  if(ns==0)
+  if(ns==0) {
     cat(sprintf("Y = %.2f +/- %.2f", short_y, short_uy))
-  else
-    if(ns <= 2 & ns > 0)
+  } else {
+    if(ns <= 2 & ns > 0) {
       if(ns == 1)
         cat(sprintf("Y = %.1f +/- %.1f", short_y*10^ns, short_uy*10^ns))
       else
         cat(sprintf("Y = %.0f +/- %.0f", short_y*10^ns, short_uy*10^ns))
-    else  
+    } else {
       cat(sprintf("Y = (%.2f +/- %.2f)*10^%d", short_y, short_uy, ns))
+    }  
+  }
+  cat(sprintf("; k = %.2f", fac))
 }
 
 #' @rdname gumPrint
@@ -68,21 +109,23 @@ CIPrint = function(y, uy, p=0.95, fac=1.96) {
   ns=floor(log10(fac*uy))+1
   short_ylow_cv =round((y-fac*uy)/10^ns,2)
   short_yhigh_cv=round((y+fac*uy)/10^ns,2)
-  if(ns==0)
+  if(ns==0) {
     cat(sprintf("\n%d percent C.I. = [%.2f, %.2f]",
                 100.0*p,short_ylow_cv,short_yhigh_cv))
-    if(ns <= 2 & ns > 0)
+  } else {
+    if(ns <= 2 & ns > 0) {
       if(ns == 1)
         cat(sprintf("\n%d percent C.I. = [%.1f, %.1f]",
                     100.0*p,short_ylow_cv*10^ns,short_yhigh_cv*10^ns))
       else
         cat(sprintf("\n%d percent C.I. = [%.0f, %.0f]",
                     100.0*p,short_ylow_cv*10^ns,short_yhigh_cv*10^ns))
-    else  
+    } else {
       cat(sprintf("\n%d percent C.I. = [%.2f, %.2f]*10^%d",
-                100.0*p,short_ylow_cv,short_yhigh_cv,ns))
+                  100.0*p,short_ylow_cv,short_yhigh_cv,ns))
+    }  
+  }
 }
-
 #' @rdname gumPrint
 #' @examples
 #' #
@@ -98,17 +141,20 @@ CIPrint1 = function(uy, y_low, y_high, p=0.95) {
   ns=floor(log10(uy))+1
   short_ylow_cv =round((y_low)/10^ns,2)
   short_yhigh_cv=round((y_high)/10^ns,2)
-  if(ns==0)
+  if(ns==0) {
     cat(sprintf("\n%d percent C.I. = [%.2f, %.2f]",
                 100.0*p,short_ylow_cv,short_yhigh_cv))
-  if(ns <= 2 & ns > 0)
-    if(ns == 1)
-      cat(sprintf("\n%d percent C.I. = [%.1f, %.1f]",
-                  100.0*p,short_ylow_cv*10^ns,short_yhigh_cv*10^ns))
-    else
-      cat(sprintf("\n%d percent C.I. = [%.0f, %.0f]",
-                100.0*p,short_ylow_cv*10^ns,short_yhigh_cv*10^ns))
-  else  
-    cat(sprintf("\n%d percent C.I. = [%.2f, %.2f]*10^%d",
-                100.0*p,short_ylow_cv,short_yhigh_cv,ns))
+  } else {
+    if(ns <= 2 & ns > 0) {
+      if(ns == 1)
+        cat(sprintf("\n%d percent C.I. = [%.1f, %.1f]",
+                    100.0*p,short_ylow_cv*10^ns,short_yhigh_cv*10^ns))
+      else
+        cat(sprintf("\n%d percent C.I. = [%.0f, %.0f]",
+                    100.0*p,short_ylow_cv*10^ns,short_yhigh_cv*10^ns))
+    } else {
+      cat(sprintf("\n%d percent C.I. = [%.2f, %.2f]*10^%d",
+                  100.0*p,short_ylow_cv,short_yhigh_cv,ns))
+    }  
+  }
 }
