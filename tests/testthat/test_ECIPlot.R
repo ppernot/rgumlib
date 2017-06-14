1bd3151eb8012aa705f059f831e8c5de3e712f2a
Pascal PERNOT <pascal.pernot@u-psud.fr>
2017-06-14 16:11:01 +0200

    Remove graphical tests

--- tests/testthat/test_ECIPlot.R
+++ tests/testthat/test_ECIPlot.R
@@ -1,14 +1,14 @@
-test_that('ECIPlot identical to reference plot',
-          {
-            set.seed(1234)
-            S = rnorm(1000)
-            png(filename = "testECIPlot.png")
-            ECIPlot(S)
-            dev.off()
-            expect_true(
-              visualTest::isSimilar(file = "testECIPlot.png",
-                        fingerprint = "80B5333BF521BF30",
-                        threshold = 0.1
-              )
-            )
-          })
+# test_that('ECIPlot identical to reference plot',
+#           {
+#             set.seed(1234)
+#             S = rnorm(1000)
+#             png(filename = "testECIPlot.png")
+#             ECIPlot(S)
+#             dev.off()
+#             expect_true(
+#               visualTest::isSimilar(file = "testECIPlot.png",
+#                         fingerprint = "80B5333BF521BF30",
+#                         threshold = 0.1
+#               )
+#             )
+#           })
--- tests/testthat/test_cumPlot.R
+++ tests/testthat/test_cumPlot.R
@@ -1,16 +1,16 @@
-test_that('cumPlot identical to reference plot',
-          {
-            set.seed(1234)
-            S = rnorm(1000)
-            png(filename = "testCumPlot.png")
-            cumPlot(S)
-            dev.off()
-            # getFingerprint("testCumPlot.png")
-            expect_true(
-              visualTest::isSimilar(file = "testCumPlot.png",
-                        fingerprint = "80F0FF03F887FA18",
-                        threshold = 0.1
-              )
-            )
-          })
-
+# test_that('cumPlot identical to reference plot',
+#           {
+#             set.seed(1234)
+#             S = rnorm(1000)
+#             png(filename = "testCumPlot.png")
+#             cumPlot(S)
+#             dev.off()
+#             # getFingerprint("testCumPlot.png")
+#             expect_true(
+#               visualTest::isSimilar(file = "testCumPlot.png",
+#                         fingerprint = "80F0FF03F887FA18",
+#                         threshold = 0.1
+#               )
+#             )
+#           })
+# 
